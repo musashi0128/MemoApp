45f9900baf86facea04f948e3086693d73d692bd
musashi0128 <k.miyamoto.0128@gmail.com>
2019-07-03 15:59:41 +0900

    react-native-start

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import BodyText from './src/elements/BodyText';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello!!!!</Text>
+      <BodyText>Hi!</BodyText>
     </View>
   );
 }
