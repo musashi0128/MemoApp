3529a0dd9f23c0a71bdcec4621190183a4ad4b12
musashi0128 <k.miyamoto.0128@gmail.com>
2019-07-06 17:52:03 +0900

    add login sigunp edit view

--- App.js
+++ App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import AppBar from './src/components/AppBar';
 //import MemoListScreen from './src/screens/MemoListScreen';
-import MemoDetailScreen from './src/screens/MemoDetailScreen';
+//import MemoDetailScreen from './src/screens/MemoDetailScreen';
+//import MemoEditScreen from './src/screens/MemoEditScreen';
+//import LoginScreen from './src/screens/LoginScreen';
+import SignupScreen from './src/screens/SignupScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <AppBar />
-      <MemoDetailScreen />
+      <SignupScreen />
     </View>
   );
 }
---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { StyleSheet, Text, TextInput, View, TouchableHighlight } from 'react-native';
+
+class LoginScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Login</Text>
+        <TextInput style={styles.input} value="Email Address" />
+        <TextInput style={styles.input} value="Password" />
+        <TouchableHighlight style={styles.button} onPress={() => {}} underlayColor="#C70F66">
+          <Text style={styles.buttonTitle}>Log in</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    padding: 24,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 28,
+    alignSelf: 'center',
+    marginBottom: 24,
+  },
+  input: {
+    backgroundColor: '#eee',
+    height: 48,
+    marginBottom: 16,
+    padding: 8,
+    borderWidth: 1,
+    borderRadius: 3,
+    borderColor: '#DDD',
+  },
+  button: {
+    backgroundColor: '#E31676',
+    height: 48,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '70%',
+    alignSelf: 'center',
+  },
+  buttonTitle: {
+    color: '#fff',
+    fontSize: 18,
+  },
+});
+
+export default LoginScreen;
